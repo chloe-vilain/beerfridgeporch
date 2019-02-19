6eb3fd7327cff27f7317b00b5ebb8d85eac3d123
cwvilain <cwvilain@gmail.com>
2019-02-19 12:51:50 -0500

    Add functions to calculate beer tempurature over time
    Add constants to Chemistry representing chemical and physical properties
    of beer, beer containers. Add functions to calculate beer tempurate
    as a function of time using Newton's Law of Cooling
    
    Add radio buttons for users to set beer container in New.

--- app/helpers/beercheck_helper.rb
+++ app/helpers/beercheck_helper.rb
@@ -4,6 +4,8 @@ module BeercheckHelper
 
   def forecast_view_generator(forecast, abv)
     forecast_view = Array.new
+    tempurature_array = Array.new
+    #beer_tempurature = Chemistry.calculate_beer_tempurature_over_time_vector(180, weather[:tempurature], 5, :bottle_12oz)
     forecast.each do |i|
       forecast_view << weather_view_generator(i, abv)
     end
@@ -17,5 +19,6 @@ module BeercheckHelper
     can_leave_out: Chemistry.can_leave_out(weather[:tempurature], abv)
     }
   end
+
 
 end
--- lib/chemistry.rb
+++ lib/chemistry.rb
@@ -1,5 +1,20 @@
 module Chemistry
   require 'logger'
+  include Math
+
+
+  #Properties of different physical bottles
+  #Surface area in m^3
+  #heat transfer coefficiant W/m^2, assumes glass thickness = 1.58 mm, aluminium thickness .36mm
+  #volume of beer, in m^3
+  @Bottle_properties = { bottle_12oz: { surface_area: 0.032607, h_coeff: 9.783, volume: 0.000354882 },
+                    can_12oz: { surface_area: 0.0311410, h_coeff: 9.557, volume: 0.000354882 },
+                    can_160z: { surface_area: 0.0393092, h_coeff: 9.557, volume: 0.000473176 }
+                  }
+
+  #properties of beer/ ethanol
+  @Density_beer = 1030 #kg/m^3 (density)
+  @Cp_beer = 4065 #J/(kg*K) (mass specific heat capacity)
 
 #Helpful functions
   def self.convert_kelvin_to_celcius(kelvin)
@@ -14,6 +29,25 @@ module Chemistry
     9/5*celcius+32
   end
 
+
+  def self.calculate_freezing_point_celcius(abv)
+    0.00-(1.45*abv.to_f/(0.05*(100.00-abv.to_f)))
+  end
+
+  def self.calculate_beer_tempurature_over_time_vector(timestep_size, t_surrounding_array, t_start, bottle_type)
+    bottle = @Bottle_properties[bottle_type]
+    r_cooling = 1/(@Cp_beer/(bottle[:h_coeff]*bottle[:volume]*@Density_beer))
+    beer_tempuratures = [t_start ]
+    t_surrounding_array.each do |t_surrounding|
+      beer_tempuratures << calculate_beer_tempurature_over_timestep(t_surrounding, beer_tempuratures.last, r_cooling, timestep_size)
+    end
+  end
+
+  #Use Newton's Law of Cooling to calculate tempurature of beer assuming constant tempurature
+  def self.calculate_beer_tempurature_over_timestep(t_surrounding, t_start, r_cooling, timestep_size)
+    t_surrounding + (t_start - t_surrounding) * exp(r_cooling * t_elapsed)
+  end
+
 #Core logic
   def self.can_leave_out(tempurature_farenheit, abv)
     if tempurature_farenheit < convert_celcius_to_farenheit(calculate_freezing_point_celcius(abv))
@@ -25,10 +59,6 @@ module Chemistry
     else
       "It is too warm to leave your beer outside"
     end
-  end
-
-  def self.calculate_freezing_point_celcius(abv)
-    0.00-(1.45*abv.to_f/(0.05*(100.00-abv.to_f)))
   end
 
 end
